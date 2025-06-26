ba0b26469649d69ffcb9076b74be76c291f396f3
hanyunguk <han4001x@naver.com>
2025-06-26 11:24:26 +0900

    최종적으로 수정

---
+++ humortube/src/App.jsx
@@ -0,0 +1,29 @@
+
+function App() {
+
+const a1 = <iframe width="560" height="315" src="https://www.youtube.com/embed/BsDXt49euc0?si=ATEaDUuANkioTsJ7&amp;start=3" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+const a2 = <iframe width="560" height="315" src="https://www.youtube.com/embed/fhs55HEl-Gc?si=81rITJJQ8ZSnssoS&amp;start=3" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+  return ( <div>
+    <center><h1>MY favourite</h1></center>
+    
+<br />
+    <center>
+    <h2>케이시믹스</h2>
+    <p>{a1}</p>
+    <h2>제이레빗</h2>
+    <p>{a2}</p>
+    <h2>옥상달빛</h2>
+    <p><iframe width="560" height="315" src="https://www.youtube.com/embed/kIL0PVJlRJE?si=znxBde0vv9Z63HNa&amp;start=5" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe></p>
+    <h2>권진아</h2>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/wW8ztKp5kp8?si=GxKoVqWwN3nEvgHJ&amp;start=5" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    <h2>심규선</h2>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/1yRyxiBpAdQ?si=Nn7vPpW-MOS7s-g5&amp;start=4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+    <h1>사진관련</h1>
+    </center>
+    </div>
+ )
+}
+
+export default App
---
+++ test.js
@@ -0,0 +1,8 @@
+let a = 10
+const b = 20
+const c = a + b
+
+a = 50
+
+//c의 값 보여주기
+console.log("c=",c)
